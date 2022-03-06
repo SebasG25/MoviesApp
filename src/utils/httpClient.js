8a7fbc89244547063352a1b72822b8d134e2d7d9
Sebastian Guzman <bitas200225@gmail.com>
2022-03-05 23:05:14 -0500

    Enviroment Variables

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,9 +1,9 @@
-const API = 'https://api.themoviedb.org/3';
+const API = process.env.REACT_APP_API;
 
 export function get(path) {
     return fetch(`${API}${path}`, {
         headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZThhYjM1OGQ3ODA4ZTIwNzk5NzNmOTEwODkyMjUwNCIsInN1YiI6IjYyMjJhMzFmODc0MWM0MDAxYjJmZTMzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TAk1dLH9_1sNJF5A0PIlXsABlhxEpSddTd4VF4qgSsk',
+            Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
             'Content-Type': 'application/json;charset=utf-8',
         },
     }).then(result => result.json());
