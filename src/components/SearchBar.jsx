f80206311d04e21f0588827c92dfa5bd74b07536
Sebastian Guzman <bitas200225@gmail.com>
2022-03-05 23:42:51 -0500

    Unused import deleted

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FaSearch } from 'react-icons/fa'
-import { useQuery } from "../hooks/useQuery";
 import styles from '../styles/SearchBar.module.css'
 
 export const SearchBar = () => {
     const [textSearched, setTextSearched] = useState('')
-
-    const query = useQuery()
-    const search = query.get('search') ?? ''
 
     const navigate = useNavigate()
 
